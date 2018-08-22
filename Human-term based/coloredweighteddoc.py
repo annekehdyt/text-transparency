72c0376c21e26431f8d4b846ff2b3907ea70e2a5
Anneke Hidayat <ahidayat@hawk.iit.edu>
2018-08-22 12:11:23 -0500

    update amazon data

--- Human-term based/coloredweighteddoc.py
+++ Human-term based/coloredweighteddoc.py
@@ -2,13 +2,12 @@ import numpy as np
 import re
 
 class ColoredWeightedDoc(object):
-    def __init__(self, doc, feature_names, coefs, token_pattern=r"(?u)\b\w\w+\b", binary = False):
+    def __init__(self, doc, human_terms, ht_weights, token_pattern=r"(?u)\b\w\w+\b", binary = False):
         self.doc = doc
-        self.feature_names = feature_names
-        self.coefs = coefs
+        self.human_terms = human_terms
+        self.ht_weights = ht_weights
         self.binary = binary
         self.tokenizer = re.compile(token_pattern)
-        self.abs_ranges = np.linspace(0, max([abs(coefs.min()), abs(coefs.max())]), 8)
     def _repr_html_(self):
         html_rep = ""
         tokens = self.doc.split(" ") 
@@ -19,85 +18,26 @@ class ColoredWeightedDoc(object):
             if len(vocab_tokens) > 0:
                 vocab_token = vocab_tokens[0]
                 try:
-                    vocab_index = self.feature_names.index(vocab_token)
+                    vocab_index = self.human_terms.index(vocab_token)
                     
                     if not self.binary or vocab_index not in seen_tokens:
                         
-                        if self.coefs[vocab_index] > 0: # positive word
-                            for i in range(1, 7):
-                                if self.coefs[vocab_index] < self.abs_ranges[i]:
-                                    break
-                            html_rep = html_rep + "<font size = " + str(i) + ", color=blue> " + token + " </font>"
+                        if self.ht_weights[vocab_index] == 0: # human-terms which has been washed out (opposing)
+                            html_rep = html_rep + "<font size = 2, color=lightgreen> " + token + " </font>"
                         
-                        elif self.coefs[vocab_index] < 0: # negative word
-                            for i in range(1, 7):
-                                if self.coefs[vocab_index] > -self.abs_ranges[i]:
-                                    break
-                            html_rep = html_rep + "<font size = " + str(i) + ", color=red> " + token + " </font>"
+                        elif self.ht_weights[vocab_index] != 0: # human-terms transparency
+                            html_rep = html_rep + "<font size = 3, color=blue> " + token + " </font>"
                         
                         else: # neutral word
-                            html_rep = html_rep + "<font size = 1, color=gray> " + token + " </font>"
+                            html_rep = html_rep + "<font size = 1, color=gainsboro> " + token + " </font>"
                         
                         if self.binary:    
                             seen_tokens.add(vocab_index)
                     
                     else: # if binary and this is a token we have seen before
-                        html_rep = html_rep + "<font size = 1, color=gray> " + token + " </font>"
+                        html_rep = html_rep + "<font size = 1, color=gainsboro> " + token + " </font>"
                 except: # this token does not exist in the vocabulary
-                    html_rep = html_rep + "<font size = 1, color=gray> " + token + " </font>"
+                    html_rep = html_rep + "<font size = 1, color=gainsboro> " + token + " </font>"
             else:
-                html_rep = html_rep + "<font size = 1, color=gray> " + token + " </font>"
-        return html_rep
-
-class ColoredWeightedDocBigram(object):
-    def __init__(self, doc, feature_names, coefs, token_pattern=r"(?u)\b\w\w+\b", binary = False):
-        self.doc = doc
-        self.feature_names = feature_names
-        self.coefs = coefs
-        self.binary = binary
-        self.tokenizer = re.compile(token_pattern)
-        self.abs_ranges = np.linspace(0, max([abs(coefs.min()), abs(coefs.max())]), 8)
-        #self.ngram_size = 2 # hard-coded
-    def _repr_html_(self):
-        html_rep = ""
-        tokens = self.doc.split(" ")
-        if self.binary:
-            seen_tokens = set()
-
-        for i in range(len(tokens)-1):
-            t1 = tokens[i]
-            t2 = tokens[i+1] # bigram
-            token = t1 + " " + t2
-            vocab_tokens = self.tokenizer.findall(token.lower())
-            if len(vocab_tokens) == 2: # hard-coded
-                vocab_token = vocab_tokens[0] + " " + vocab_tokens[1]
-                try:
-                    vocab_index = self.feature_names.index(vocab_token)
-                    
-                    if not self.binary or vocab_index not in seen_tokens:
-                        
-                        if self.coefs[vocab_index] > 0: # positive word
-                            for i in range(1, 7):
-                                if self.coefs[vocab_index] < self.abs_ranges[i]:
-                                    break
-                            html_rep = html_rep + "<font size = " + str(i) + ", color=blue> [" + token + "] </font>"
-                        
-                        elif self.coefs[vocab_index] < 0: # negative word
-                            for i in range(1, 7):
-                                if self.coefs[vocab_index] > -self.abs_ranges[i]:
-                                    break
-                            html_rep = html_rep + "<font size = " + str(i) + ", color=red> [" + token + "] </font>"
-                        
-                        else: # neutral word
-                            html_rep = html_rep + "<font size = 1, color=gray> [" + token + "] </font>"
-                        
-                        if self.binary:    
-                            seen_tokens.add(vocab_index)
-                    
-                    else: # if binary and this is a token we have seen before
-                        html_rep = html_rep + "<font size = 1, color=gray> [" + token + "] </font>"
-                except: # this token does not exist in the vocabulary
-                    html_rep = html_rep + "<font size = 1, color=gray> [" + token + "] </font>"
-            else:
-                html_rep = html_rep + "<font size = 1, color=gray> [" + token + "] </font>"
+                html_rep = html_rep + "<font size = 1, color=gainsboro> " + token + " </font>"
         return html_rep
