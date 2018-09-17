687fad61a4cf2dfe4d4654d9f4f44f780462d773
Anneke Hidayat <ahidayat@hawk.iit.edu>
2018-09-17 17:20:45 -0500

    update

--- Human-term based/lr_baseline.py
+++ Human-term based/lr_baseline.py
@@ -1,5 +1,8 @@
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import log_loss
+
+import numpy as np
 
 class LRBaseline():
     def __init__(self, X_train_corpus, X_test_corpus, y_train, y_test, human_terms=None, min_df=100,
@@ -27,10 +30,12 @@ class LRBaseline():
         X_train = self.baseline_cv.fit_transform(self.X_train_corpus)
         X_test = self.baseline_cv.transform(self.X_test_corpus)
 
-        clf = LogisticRegression(random_state=self.random_state, penalty=penalty)
-        clf.fit(X_train, self.y_train)
+        self.clf_base = LogisticRegression(random_state=self.random_state, penalty=penalty)
+        self.clf_base.fit(X_train, self.y_train)
 
-        return clf.score(X_train, self.y_train), clf.score(X_test, self.y_test)
+        p = self.clf_base.predict_proba(X_test)[:,1]
+
+        return self.clf_base.score(X_train, self.y_train), self.clf_base.score(X_test, self.y_test), log_loss(self. y_test, p)
 
 
     def human_terms_baseline(self, penalty='l2'):
@@ -39,7 +44,13 @@ class LRBaseline():
         X_train = self.human_terms_cv.fit_transform(self.X_train_corpus)
         X_test = self.human_terms_cv.transform(self.X_test_corpus)
 
-        clf = LogisticRegression(random_state=self.random_state, penalty=penalty)
-        clf.fit(X_train, self.y_train)
 
-        return clf.score(X_train, self.y_train), clf.score(X_test, self.y_test)
+        indices = np.where(np.sum(X_test, axis=1) != 0)[0]
+        r_rate = (X_test.shape[0] - len(indices))/X_test.shape[0]
+
+        self.clf_human = LogisticRegression(random_state=self.random_state, penalty=penalty)
+        self.clf_human.fit(X_train, self.y_train)
+
+        p = self.clf_human.predict_proba(X_test[indices])[:,1]
+
+        return self.clf_human.score(X_train, self.y_train), self.clf_human.score(X_test[indices], self.y_test[indices]), log_loss(self.y_test[indices], p), r_rate
